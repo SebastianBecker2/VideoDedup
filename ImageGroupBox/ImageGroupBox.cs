34d83fe7e64df5d57f242217c0eaa79c0b3d5218
Sebastian Becker <mail@sbecker.de.com>
2021-12-12 20:30:47 +0100

    Fix ImageGroupBox header size

--- ImageGroupBox/ImageGroupBox.cs
+++ ImageGroupBox/ImageGroupBox.cs
@@ -14,11 +14,56 @@ namespace ImageGroupBox
         private VisualStyleRenderer renderer = null;
 
         /// <summary>Get or set the icon to be painted in the header area.</summary>
-        [Description("Icon before the text"), AmbientValue(null), Category("Appearance"), Localizable(true)]
+        [Description("Icon before the text"),
+            AmbientValue(null),
+            Category("Appearance"),
+            Localizable(true)]
         public Image Icon
         {
             get => icon;
-            set { if (icon != value) { icon = value; Invalidate(false); } }
+            set
+            {
+                if (icon != value)
+                {
+                    icon = value;
+                    Invalidate(false);
+                }
+            }
+        }
+
+        private int fontHeight = -1;
+        private Font cachedFont;
+        /// <summary>Gets a rectangle that represents the dimensions fo the GroupBox.</summary>
+        public override Rectangle DisplayRectangle
+        {
+            get
+            {
+                var size = ClientSize;
+
+                if (fontHeight == -1)
+                {
+                    fontHeight = Font.Height;
+                    cachedFont = Font;
+                }
+                else if (!ReferenceEquals(cachedFont, Font))
+                {
+                    // Must also cache font identity here because
+                    // we need to provide an accurate DisplayRectangle
+                    // picture even before the OnFontChanged event bubbles
+                    // through.
+                    fontHeight = Font.Height;
+                    cachedFont = Font;
+                }
+
+                // For efficiency, so that we don't need to read property store four times.
+                var padding = Padding;
+                var headerHeight = Math.Max(fontHeight, Icon.Height);
+                return new Rectangle(
+                    padding.Left,
+                    headerHeight + padding.Top,
+                    Math.Max(size.Width - padding.Horizontal, 0),
+                    Math.Max(size.Height - headerHeight - padding.Vertical, 0));
+            }
         }
 
         /// <summary>Override the GroupBox OnPaint method for customized drawing.</summary>
@@ -26,10 +71,14 @@ namespace ImageGroupBox
         protected override void OnPaint(PaintEventArgs e)
         {
             // This override only draws the control if the icon is not null. Otherwise, the base method is called
-            if (icon != null && Application.RenderWithVisualStyles && (Width >= 10) && (Height >= 10))
+            if (icon != null
+                && Application.RenderWithVisualStyles
+                && (Width >= 10)
+                && (Height >= 10))
             {
                 // Draw the entire control
                 DrawGroupBox(e.Graphics);
+                e.Graphics.Dispose();
             }
             else
             {
@@ -38,52 +87,81 @@ namespace ImageGroupBox
         }
 
         /// <summary>Draw the entire control with visual styles effects.</summary>
-        /// <param name="grfx">The Graphics object in which the control must painted to.</param>
-        private void DrawGroupBox(Graphics grfx)
+        /// <param name="graphics">The Graphics object in which the control must painted to.</param>
+        private void DrawGroupBox(Graphics graphics)
         {
-            // Set the enabled state of the control
-            var state = Enabled ? GroupBoxState.Normal : GroupBoxState.Disabled;
             // Set the flags of TextFormat
-            var txtflags = TextFormatFlags.PreserveGraphicsTranslateTransform | TextFormatFlags.PreserveGraphicsClipping | TextFormatFlags.TextBoxControl | TextFormatFlags.WordBreak;
+            var textFlags = TextFormatFlags.PreserveGraphicsTranslateTransform
+                | TextFormatFlags.PreserveGraphicsClipping
+                | TextFormatFlags.TextBoxControl
+                | TextFormatFlags.WordBreak;
             if (!ShowKeyboardCues)
             {
-                txtflags |= TextFormatFlags.HidePrefix;
+                textFlags |= TextFormatFlags.HidePrefix;
             }
-
             if (RightToLeft == RightToLeft.Yes)
             {
-                txtflags |= TextFormatFlags.RightToLeft | TextFormatFlags.Right;
+                textFlags |= TextFormatFlags.RightToLeft | TextFormatFlags.Right;
             }
-            // The rectangle that bounds the control
-            var bounds = new Rectangle(0, 0, Width, Height);
+
             // Initialize the renderer for visual styles
+            var state = Enabled ? GroupBoxState.Normal : GroupBoxState.Disabled;
             InitializeRenderer((int)state);
-            // Set the rectangle to display the Text
-            var txtsize = TextRenderer.MeasureText(grfx, base.Text, base.Font, new Size(bounds.Width - 14, bounds.Height));
-            // The optimized height of the header
-            var headerheight = Math.Max(icon.Height, txtsize.Height);
-            // Define the rectangle for the icon
-            var iconrect = new Rectangle(9, (headerheight - icon.Height) / 2, icon.Width, icon.Height);
-            // Define the rectangle of the text
-            var textrect = new Rectangle(new Point(iconrect.Right, (headerheight - txtsize.Height) / 2), txtsize);
-            // Move the rectangles if needed by the txtflags
-            if ((txtflags & TextFormatFlags.Right) == TextFormatFlags.Right)
+
+            var textSize = TextRenderer.MeasureText(
+                graphics,
+                base.Text,
+                base.Font,
+                new Size(Width - 14, Height));
+
+            var headerRectangle = new Rectangle(
+                0,
+                0,
+                Width,
+                Math.Max(icon.Height, textSize.Height));
+
+            var iconRectangle = new Rectangle(
+                9,
+                (headerRectangle.Height - icon.Height) / 2,
+                icon.Width,
+                icon.Height);
+
+            var textRactangle = new Rectangle(
+                iconRectangle.Right,
+                (headerRectangle.Height - textSize.Height) / 2,
+                textSize.Width,
+                textSize.Height);
+
+            // Move the rectangles if needed by the textFlags
+            if ((textFlags & TextFormatFlags.Right) == TextFormatFlags.Right)
             {
-                iconrect.X = bounds.Right - iconrect.Right - 1;
-                textrect.X = bounds.Right - textrect.Right - 1;
+                iconRectangle.X = Width - iconRectangle.Right - 1;
+                textRactangle.X = Width - textRactangle.Right - 1;
             }
-            // Define the rectangle that defines the inner container
-            var displayrect = bounds;
-            displayrect.Y += headerheight / 2;
-            displayrect.Height -= headerheight / 2;
-            // Draw the icon
-            DrawIcon(grfx, icon, iconrect, state);
-            // Draw the text
-            DrawText(grfx, Text, Font, textrect, renderer.GetColor(ColorProperty.TextColor), BackColor, txtflags);
-            // Draw the background
-            DrawBackground(grfx, displayrect, textrect, icon.Width, txtflags);
-            // Clean up
-            grfx.Dispose();
+
+            var displayRectangle = new Rectangle(
+                0,
+                headerRectangle.Height / 2,
+                Width,
+                Height - (headerRectangle.Height / 2));
+
+            DrawIcon(graphics, icon, iconRectangle, state);
+
+            TextRenderer.DrawText(
+                graphics,
+                Text,
+                Font,
+                textRactangle,
+                renderer.GetColor(ColorProperty.TextColor),
+                BackColor,
+                textFlags);
+
+            DrawBackground(
+                graphics,
+                displayRectangle,
+                headerRectangle,
+                icon.Width,
+                textFlags);
         }
 
         /// <summary>Draw an icon in a enabled or disabled state.</summary>
@@ -91,12 +169,21 @@ namespace ImageGroupBox
         /// <param name="icon">The icon to be painted</param>
         /// <param name="rc">The rectangle that bounds the icon.</param>
         /// <param name="state">Specifies whether the icon must be painted in a disabled or endabled state.</param>
-        private void DrawIcon(Graphics grfx, Image icon, Rectangle rc, GroupBoxState state)
+        private void DrawIcon(
+            Graphics grfx,
+            Image icon,
+            Rectangle rc,
+            GroupBoxState state)
         {
             if (state == GroupBoxState.Disabled)
             {
                 // Draw the disabled icon
-                ControlPaint.DrawImageDisabled(grfx, icon, rc.Left, rc.Top, Color.Empty);
+                ControlPaint.DrawImageDisabled(
+                    grfx,
+                    icon,
+                    rc.Left,
+                    rc.Top,
+                    Color.Empty);
             }
             else
             {
@@ -105,24 +192,18 @@ namespace ImageGroupBox
             }
         }
 
-        /// <summary>Draw a text.</summary>
-        /// <param name="grfx">The Graphics object in which the text must drawn to.</param>
-        /// <param name="text">The text to draw.</param>
-        /// <param name="font">The font used to draw the text.</param>
-        /// <param name="bounds">The rectangle that bounds the text.</param>
-        /// <param name="txtcolor">The fore color of the text.</param>
-        /// <param name="backcolor">The color of the background of the text.</param>
-        /// <param name="txtflags">Attributes to format the text.</param>
-        private void DrawText(Graphics grfx, string text, Font font, Rectangle bounds, Color txtcolor, Color backcolor, TextFormatFlags txtflags) =>
-            TextRenderer.DrawText(grfx, text, font, bounds, txtcolor, backcolor, txtflags);
-
         /// <summary>Draw the rounded rectangle of the control.</summary>
         /// <param name="grfx">The Graphics object in which the rectangle must drawn to.</param>
         /// <param name="bounds">The rectangle that bounds the rounded rectangle.</param>
         /// <param name="headerrect">The rectangle that bounds the header area.</param>
         /// <param name="iconwidth">The width of the icon of the header.</param>
         /// <param name="txtflags"></param>
-        private void DrawBackground(Graphics grfx, Rectangle bounds, Rectangle headerrect, int iconwidth, TextFormatFlags txtflags)
+        private void DrawBackground(
+            Graphics grfx,
+            Rectangle bounds,
+            Rectangle headerrect,
+            int iconwidth,
+            TextFormatFlags txtflags)
         {
             var leftrect = bounds;
             leftrect.Width = 7;
@@ -143,7 +224,6 @@ namespace ImageGroupBox
             }
             middlerect.Y = headerrect.Bottom;
             middlerect.Height -= headerrect.Bottom - bounds.Top;
-            //VisualStyleRenderer visualstylerenderer = this.CreateRenderer((int)state);
             // Left part
             renderer.DrawBackground(grfx, bounds, leftrect);
             // Middle part
@@ -159,11 +239,17 @@ namespace ImageGroupBox
             var visualstyleelement = VisualStyleElement.Button.GroupBox.Normal;
             if (renderer == null)
             {
-                renderer = new VisualStyleRenderer(visualstyleelement.ClassName, visualstyleelement.Part, state);
+                renderer = new VisualStyleRenderer(
+                    visualstyleelement.ClassName,
+                    visualstyleelement.Part,
+                    state);
             }
             else
             {
-                renderer.SetParameters(visualstyleelement.ClassName, visualstyleelement.Part, state);
+                renderer.SetParameters(
+                    visualstyleelement.ClassName,
+                    visualstyleelement.Part,
+                    state);
             }
         }
 
