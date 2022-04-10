f28ebd2195eb7e38bd5f1c3e1ccc79bc84cbcffa
Sebastian Becker <mail@sbecker.de.com>
2022-04-11 00:49:40 +0200

    Fix formatting

--- VideoDedupClient/Program.cs
+++ VideoDedupClient/Program.cs
@@ -104,8 +104,6 @@ namespace VideoDedupClient
                 Settings.Default.Save();
             }
 
-
-
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
