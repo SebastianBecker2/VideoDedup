b9cd0a5c7d9b91ea3b648455edda8e92577beccf
Sebastian Becker <mail@sbecker.de.com>
2022-04-11 00:47:58 +0200

    Specify backoff behavior for grpc client
    The client uses a specific backoff configuration for all messages except
    the GetCurrentStatus message. The default is 5 retries with an initial
    delay of up to 1 second, a multiplier of 1.2 and a maximum backoff of 5
    seconds for StatusCode Unavailable. For the GetCurrentStatus message,
    it's 2 retries with an initial delay of up to 0.1 seconds with a
    multiplier of 1 and a maximum backoff of 0.1 seconds for StatusCode
    Unavailable.

--- VideoDedupClient/Program.cs
+++ VideoDedupClient/Program.cs
@@ -1,7 +1,9 @@
 namespace VideoDedupClient
 {
     using Dialogs;
+    using Grpc.Core;
     using Grpc.Net.Client;
+    using Grpc.Net.Client.Configuration;
     using Properties;
     using static VideoDedupGrpc.VideoDedupGrpcService;
 
@@ -9,6 +11,39 @@ namespace VideoDedupClient
     {
         private static GrpcChannel? grpcChannel;
         private static readonly object GrpcClientLock = new();
+        private static readonly MethodConfig GrpcDefaultBackoffConfig =
+            new()
+            {
+                Names = { MethodName.Default },
+                RetryPolicy = new RetryPolicy
+                {
+                    MaxAttempts = 5,
+                    InitialBackoff = TimeSpan.FromSeconds(1),
+                    MaxBackoff = TimeSpan.FromSeconds(5),
+                    BackoffMultiplier = 1.2,
+                    RetryableStatusCodes = { StatusCode.Unavailable }
+                },
+            };
+        private static readonly MethodConfig GrpcGetCurrentStatusBackoffConfig =
+            new()
+            {
+                Names =
+                {
+                    new MethodName
+                    {
+                        Service = "video_dedup_grpc.VideoDedupGrpcService",
+                        Method = "GetCurrentStatus",
+                    },
+                },
+                RetryPolicy = new RetryPolicy
+                {
+                    MaxAttempts = 2,
+                    InitialBackoff = TimeSpan.FromMilliseconds(100),
+                    MaxBackoff = TimeSpan.FromMilliseconds(100),
+                    BackoffMultiplier = 1,
+                    RetryableStatusCodes = { StatusCode.Unavailable }
+                },
+            };
         internal static VideoDedupGrpcServiceClient GrpcClient
         {
             get
@@ -20,6 +55,14 @@ namespace VideoDedupClient
                         new GrpcChannelOptions
                         {
                             MaxReceiveMessageSize = null,
+                            ServiceConfig = new ServiceConfig
+                            {
+                                MethodConfigs =
+                                {
+                                    GrpcDefaultBackoffConfig,
+                                    GrpcGetCurrentStatusBackoffConfig,
+                                },
+                            }
                         });
                     return new VideoDedupGrpcServiceClient(grpcChannel);
                 }
@@ -60,6 +103,8 @@ namespace VideoDedupClient
                 Settings.Default.UpgradeRequired = false;
                 Settings.Default.Save();
             }
+
+
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
