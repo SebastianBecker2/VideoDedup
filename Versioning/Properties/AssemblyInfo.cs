7830811e021399f234288c5f48a40392760a5007
Sebastian Becker <mail@sbecker.de.com>
2022-04-12 00:27:52 +0200

    Update Versioning project to .NET 6

--- Versioning/Properties/AssemblyInfo.cs
+++ Versioning/Properties/AssemblyInfo.cs
@@ -1,43 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Versioning")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Versioning")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 [assembly: AssemblyProduct("Versioning")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Versioning")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("80d1c901-fbd3-422a-b810-345c43edfc37")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-// -------------------------------------------
-// -------------------------------------------
-// We don't sepcify AssemblyVersion and AssemblyFileVersion here
-// Since we use this projects to specify the AssemblyVersion
-// and AssemblyFileVersion using a T4 template for the whole
-// solution.
-// -------------------------------------------
-// -------------------------------------------
+// We use the versio specified in the Versioning.cs instead. So that we can
+// link it in other files and have the same version in all projects.
 //[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
