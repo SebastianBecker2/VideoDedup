fb695a4153cd6c836fa2102edc46f49ce2de8456
Sebastian Becker <mail@sbecker.de.com>
2025-05-23 12:40:59 +0200

    Remove warning about sealable classes

--- FfmpegLib/Frame.cs
+++ FfmpegLib/Frame.cs
@@ -3,7 +3,7 @@ namespace FfmpegLib
     using System;
     using FFmpeg.AutoGen;
 
-    internal unsafe class Frame : IDisposable, IFrame
+    internal sealed unsafe class Frame : IDisposable, IFrame
     {
         private AVFrame* framePtr = ffmpeg.av_frame_alloc();
         private bool disposedValue;
@@ -64,7 +64,7 @@ namespace FfmpegLib
             framePtr = null;
         }
 
-        protected virtual void Dispose(bool disposing)
+        private void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
